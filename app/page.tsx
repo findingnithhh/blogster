0de6dcbfd6c6ff83d9d86ae76ad31c7b8e73b5aa
SOMKEY\ROG <manithjr1331@gmail.com>
2024-04-29 00:57:07 +0700

    added aceternity components

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,14 @@
 import Navbar from "@/components/Navbar";
 import { Button } from "@/components/ui/button";
-import Hero from "@/components/ui/Hero";
+import { CardDirection } from "@/components/CardDirection";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <>
-    <Navbar></Navbar>
-    <Hero />
+      <Navbar></Navbar>
+      <CardDirection />
+
     </>
   );
 }
